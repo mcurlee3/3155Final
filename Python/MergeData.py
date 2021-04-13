f5a1239bdd555bad847ccbdb6d0735a0d2578a84
Charlie Jubera <cjubera@uncc.edu>
2021-04-13 11:18:37 -0400

    update mergedata.py

--- Python/MergeData.py
+++ Python/MergeData.py
@@ -1,1 +1,18 @@
 import pandas as pd
+
+df_main = pd.read_csv("Data - General Data.csv")
+df_locations = pd.read_csv("Data - Locations.csv")
+
+df_main = df_main.sort_values('Country name')
+
+df_main.head()
+
+df_locations.drop(["country"],axis=1,inplace=True)
+
+df_locations = df_locations.rename(columns={'name':'Country name'})
+
+df_merge = pd.merge(df_main,df_locations, on='Country name')
+
+file = open('Merged.csv', 'w')
+file.write(df_merge.to_csv())
+file.close()
