aafe76096b989a4757c360dda417ea05f8f89384
Charlie Jubera <cjubera@uncc.edu>
2021-04-13 15:58:47 -0400

    Update MergeData.py

--- Python/MergeData.py
+++ Python/MergeData.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 df_main = pd.read_csv("Data - General Data.csv")
 df_locations = pd.read_csv("Data - Locations.csv")
+df_locations = pd.read_csv("Data - Locations.csv")
+df_cost = pd.read_csv("Cost of Living.csv")
 
 df_main = df_main.sort_values('Country name')
 
@@ -13,6 +15,11 @@ df_locations = df_locations.rename(columns={'name':'Country name'})
 
 df_merge = pd.merge(df_main,df_locations, on='Country name')
 
+df_merge_v2 = pd.merge(df_merge, df_cost, on='Country name')
+
 file = open('Merged.csv', 'w')
+file.write(df_merge.to_csv())
+file.close()
+file = open('Merged_v2.csv', 'w')
 file.write(df_merge.to_csv())
 file.close()
